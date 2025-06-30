fdf7444e87d302936fe003c57b420771320b0295
zehan12 <zehan9211@gmail.com>
2025-06-30 09:09:30 +0530

    feat: :sparkles: add font and header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
 export default function Home() {
   return (
-    <></>
+    <>
+      <main className="flex-grow container mx-auto px-4 py-8 md:px-6 md:py-12">
+        <header className="text-center mb-10 md:mb-16">
+          <div className="inline-flex items-center gap-3 mb-2">
+            <div className="md:text-6xl">
+              ⏳
+            </div>
+            <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl text-inter font-bold text-transparent md:text-7xl">
+              Quantum Verse
+            </h1>
+          </div>
+          <p className="text-lg md:text-xl text-muted-foreground font-body max-w-2xl mx-auto">
+            Step into the Quantum Verse — Witness technology through the eyes of time
+          </p>
+        </header>
+      </main>
+    </>
   );
 }
